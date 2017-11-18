9a823cfe223787f9accab8af56b3a5051f27cf0b
mobin <996833461@qq.com>
2017-11-18 23:10:06 +0800

    使用fastJson作为json框架

---
+++ src/main/java/com/mobin/config/WebMvcConfigurer.java
@@ -0,0 +1,28 @@
+package com.mobin.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullBooleanAsFalse);
+        converter.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        converter.setFastJsonConfig(config);
+        converters.add(converter);
+
+    }
+}
