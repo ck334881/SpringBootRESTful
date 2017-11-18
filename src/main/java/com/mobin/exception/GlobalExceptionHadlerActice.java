b989057f830b596404ef04bd6d4a568601f5be7b
mobin <996833461@qq.com>
2017-11-18 23:11:19 +0800

    定义全局异常

--- src/main/java/com/mobin/exception/GlobalExceptionHadlerActice.java
+++ src/main/java/com/mobin/exception/GlobalExceptionHadlerActice.java
@@ -5,6 +5,7 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * Created by Mobin on 2017/11/16.
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHadlerActice {
     private static final long serialVersionUID = 1L;
-    @ExceptionHandler(value = URLIllegalArgumentException.class)
-    public ErrorMessage urlillegalArgumentException(HttpServletRequest request, Exception e){
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    public ErrorMessage NoHandlerFoundException(HttpServletRequest request, Exception e){
         ErrorMessage errorMessage = new ErrorMessage();
         errorMessage.setStatus(HttpStatus.NOT_FOUND.value());
         errorMessage.setMessage(e.getLocalizedMessage());
