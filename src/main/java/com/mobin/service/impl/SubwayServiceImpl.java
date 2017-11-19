babede977a74847196b6a484eca5359025d8091e
mobin <996833461@qq.com>
2017-11-19 17:57:45 +0800

    添加分页语句

--- src/main/java/com/mobin/service/impl/SubwayServiceImpl.java
+++ src/main/java/com/mobin/service/impl/SubwayServiceImpl.java
@@ -3,6 +3,7 @@ package com.mobin.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.mobin.dao.SubwayMapper;
 import com.mobin.entity.Subway;
+import com.mobin.exception.EntityNotFoundException;
 import com.mobin.service.SubwayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,11 +22,13 @@ public class SubwayServiceImpl implements SubwayService {
     private SubwayMapper subwayMapper;
 
     public Subway findSubwayByID(int id) {
-        return subwayMapper.findSubwayByID(id);
+
+        Subway subway = subwayMapper.findSubwayByID(id);
+        return subway;
     }
 
     public List<Subway> findSubways(int pageNum,int  pageSize){
-        PageHelper.startPage(pageNum,pageSize);
+        PageHelper.startPage(pageNum,pageSize,false);
         return subwayMapper.findSubways();
     }
 }
