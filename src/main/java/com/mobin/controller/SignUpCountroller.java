b4f30ffbf24468c2bacc53c864b044bf892b7188
mobin <996833461@qq.com>
2017-11-24 17:57:21 +0800

    添加了SignUpController

--- src/main/java/com/mobin/controller/SignUpCountroller.java
+++ src/main/java/com/mobin/controller/SignUpCountroller.java
@@ -4,8 +4,13 @@ package com.mobin.controller;
  * Created by Mobin on 2017/11/22.
  */
 
+import com.mobin.VO.UserVO;
+import com.mobin.entity.User;
+import com.mobin.service.UserService;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/signUp")
 public class SignUpCountroller {
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public UserVO signUp(){
+        User user = new User();
+        user.setUserName("mobin");
+        //TODO 加密
+        user.setPassWord("154283");
+       return service.createUser(user);
+    }
 }
